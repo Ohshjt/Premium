b2ce5a7addde0434b7e69697dd1421abbd130bfd
ie1w3n <Ohshjt125@gmail.com>
2026-01-14 18:03:55 +0700

    Update deleteHeader.js

--- js/Locket/deleteHeader.js
+++ js/Locket/deleteHeader.js
@@ -1,27 +1,11 @@
 const version = 'V1.0.3';
 
-function setHeaderValue(headers, key, value) {
-    const lowerKey = key.toLowerCase();
-    if (lowerKey in headers) {
-        headers[lowerKey] = value;
-    } else {
-        headers[key] = value;
-    }
+function setHeaderValue(e, a, d) {
+  var r = a.toLowerCase();
+  r in e ? e[r] = d : e[a] = d;
 }
 
 var modifiedHeaders = $request.headers;
-
-const headersToModify = {
-    "X-RevenueCat-ETag": "",
-    "If-None-Match": "",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache"
-};
-
-for (let key in headersToModify) {
-    setHeaderValue(modifiedHeaders, key, headersToModify[key]);
-}
-
-console.log("🛠 Modified Headers:", JSON.stringify(modifiedHeaders, null, 2));
-
+setHeaderValue(modifiedHeaders, "X-RevenueCat-ETag", "");
+console.log("Modified Headers:", JSON.stringify(modifiedHeaders));
 $done({ headers: modifiedHeaders });
